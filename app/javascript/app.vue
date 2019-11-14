59077d604b2f526bb410c09b1be56ef82e20fb2f
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-14 18:55:57 +0900

    [update] vue-routerでトップ画面表示

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,21 +1,30 @@
 <template>
-  <div id="app">
-    <p class="text-center">{{ message }}</p>
+  <div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
-    }
+  import Vue from 'vue'
+  import VueRouter from 'vue-router'
+
+  import HomeIndex from 'home/index.vue'
+
+  const router = new VueRouter({
+    routes: [
+      {
+        path: '/',
+        component: HomeIndex
+      }
+    ]
+  })
+
+  Vue.use(VueRouter)
+
+  export default {
+    router
   }
-}
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-}
 </style>
---
+++ app/javascript/home/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>{{ text }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        text: 'Welcome to cmpl!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
