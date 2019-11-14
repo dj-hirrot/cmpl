5350a6684cf11ecdcf2b675ae8a53633828d0cfe
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-14 18:31:28 +0900

    [update] api用のroute設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  namespace :api, { format: :json } do
+    namespace :v1 do
+      devise_for :users
+    end
+  end
+
   root 'home#index'
 end
