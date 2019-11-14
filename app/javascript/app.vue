b1986f424f92bca3dc0e1f56bba344b94de9516f
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-14 16:51:36 +0900

    [first commit]

---
+++ app/javascript/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <p class="text-center">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+}
+</style>
