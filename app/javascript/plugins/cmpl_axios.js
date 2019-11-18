d4541ccb261246aa69823eda8d8712f98073d6ea
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-18 16:09:41 +0900

    [update] CSRF対策のPlugInを実装

---
+++ app/javascript/plugins/cmpl_axios.js
@@ -0,0 +1,19 @@
+const CMPLAxiosPlugin = {}
+
+export default CMPLAxiosPlugin.install = function(Vue, { axios }) {
+  const csrf_token = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+  axios.defaults.headers.common = {
+    "X-Requested-With": "XMLHttpRequest",
+    "X-CSRF-Token": csrf_token
+  }
+
+  Vue.axios = axios
+
+  Object.defineProperties(Vue.prototype, {
+    axios: {
+      get () {
+        return axios
+      }
+    }
+  })
+}
