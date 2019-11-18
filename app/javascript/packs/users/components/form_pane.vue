7d1313d95a605c78565153e2302267e85c2d64b9
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-18 16:09:09 +0900

    [update] フォームを追加

--- app/javascript/packs/users/components/form_pane.vue
+++ app/javascript/packs/users/components/form_pane.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>
-    <button @click.prevent="$emit('click')">function</button>
-  </div>
+  <form @submit.prevent="$emit('submit')">
+    <div v-if="errors.length != 0">
+      <ul v-for="e in errors" :key="e">
+        <li>{{ e }}</li>
+      </ul>
+    </div>
+
+    <div>
+      <label>USERNAME</label>
+      <input v-model="user.username" type="text">
+    </div>
+
+    <div>
+      <label>EMAIL</label>
+      <input v-model="user.email" type="email">
+    </div>
+
+    <div>
+      <label>PASSWORD</label>
+      <input v-model="user.password" type="password">
+    </div>
+
+    <div>
+      <button type="submit">JOIN!</button>
+    </div>
+  </form>
 </template>
 
 <script>
