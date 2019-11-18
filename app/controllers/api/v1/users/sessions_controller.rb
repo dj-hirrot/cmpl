2e60ea66351e6b2c81b1fb3ee8cc7502728fbbb5
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-18 21:37:59 +0900

    [update] sessionコントローラの修正

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,27 +1,24 @@
 # frozen_string_literal: true
 
 class Api::V1::Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  # POST /users/sign_in
+  def create
+    super
+  end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
 
-  # protected
+  protected
+    def session_params
+      param.require(:session).permit(session_param_attributes)
+    end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    def session_param_attributes
+      [
+        :username, :email, :password
+      ]
+    end
 end
