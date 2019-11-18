864eef32f1eb4b25eb8c686956f861ed43846c78
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-18 16:07:25 +0900

    [update] カスタマイズしたdeviseコントローラを使用する

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace :api, { format: :json } do
     namespace :v1 do
-      devise_for :users
+      devise_for :users, controllers: {
+        registrations: 'api/v1/users/registrations',
+        sessions: 'api/v1/users/sessions'
+      }
     end
   end
 
