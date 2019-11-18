be2f027d5b4195321a1c274d8cd6cc40d02ee995
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-18 18:24:39 +0900

    [update] 不要な検証は削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
          :confirmable, :trackable
 
   validates :username, presence: true, length: { minimum: 5, maximum: 15 }, uniqueness: { case_sensitive: true }, format: { with: /\A[a-zA-Z0-9]+\z/ }
-  validates :email,    presence: true, uniqueness: { case_sensitive: true }
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :email,    uniqueness: { case_sensitive: true }
+  validates :password, length: { minimum: 10 }
 end
