3d844a92391877c7e017c57cd34ae46ae8c3f60b
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-18 12:57:46 +0900

    [update] フォームのパーシャルを表示しつつ関数を渡す

---
+++ app/javascript/packs/users/form_pane.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button @click.prevent="$emit('click')">function</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      user: {},
+      errors: ''
+    }
+  }
+</script>
+
+<style scoped>
+</style>
--- app/javascript/packs/users/new.vue
+++ app/javascript/packs/users/new.vue
@@ -1,8 +1,35 @@
 <template>
-  <div>test</div>
+  <div>
+    <h1>Signup</h1>
+    <form-pane :errors="errors" :user="user" @click="createUser"></form-pane>
+  </div>
 </template>
 
 <script>
+  import axios from 'axios'
+  import FormPane from './form_pane.vue'
+
+  export default {
+    components: {
+      FormPane
+    },
+    data() {
+      return {
+        user: {
+          username: '',
+          email: '',
+          password: '',
+          password_confirmatioin: ''
+        },
+        errors: ''
+      }
+    },
+    methods: {
+      createUser: function() {
+        console.log('createUser')
+      }
+    }
+  }
 </script>
 
 <style scoped>
