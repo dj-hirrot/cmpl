14b74a60e9b09dc1e1a262bc082f834ab0494fef
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-18 17:52:32 +0900

    [update] validationの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :trackable
+
+  validates :username, presence: true, length: { minimum: 5, maximum: 15 }, uniqueness: { case_sensitive: true }, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :email,    presence: true, uniqueness: { case_sensitive: true }
+  validates :password, presence: true, length: { minimum: 8 }
 end
