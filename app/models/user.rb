2d016321919ff297fb27763ecc9ce1304de68c8d
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-18 23:19:06 +0900

    [update] devise_token_authを使用する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
+  # :confirmable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :trackable
+         :trackable
 
-  validates :username, presence: true, length: { minimum: 5, maximum: 15 }, uniqueness: { case_sensitive: true }, format: { with: /\A[a-zA-Z0-9]+\z/ }
-  validates :email,    uniqueness: { case_sensitive: true }
   validates :password, length: { minimum: 10 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, { format: :json } do
-    namespace :v1 do
-      devise_for :users, controllers: {
-        registrations: 'api/v1/users/registrations',
-        sessions: 'api/v1/users/sessions'
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
       }
     end
   end
