ac036f39cc9c25903b4d65068fb6dbc2fa0c3365
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-20 00:30:02 +0900

    [update] ログイン設定周り

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,4 +1,15 @@
 class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
+  def create
+    # TODO: [dj-hirrot] emailかusernameか判断してkey名を切り替える
+    if params[:session][:login].include?('@')
+      params[:session][:email] = params[:session].delete(:login)
+    else
+      params[:session][:username] = params[:session].delete(:login)
+    end
+
+    super
+  end
+
   private
     def resource_params
       params.require(:session).permit(:login, :password)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :trackable, :confirmable, authentication_keys: [:login]
+         :trackable, :confirmable, authentication_keys: [:username, :email]
 
   validates :username, presence: true,
                        uniqueness: { case_sensitive: true },
