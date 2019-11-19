f46e077b4993a203963885e2a0ac939ed3d3b4bb
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-19 22:36:22 +0900

    [fix] プロパティ名を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :trackable, :confirmable, authentication_keys: [:login]
 
-  validates :name, presence: true,
-                   uniqueness: { case_sensitive: true },
-                   length: { minimum: 5, maximum: 20 },
-                   format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: true },
+                       length: { minimum: 5, maximum: 20 },
+                       format: { with: /\A[a-zA-Z0-9]+\z/ }
   validates :password, length: { minimum: 10 }
 
   include DeviseTokenAuth::Concerns::User
