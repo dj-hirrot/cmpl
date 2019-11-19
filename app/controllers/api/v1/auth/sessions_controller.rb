a26af26cab356314de4b5244954f932c1b244082
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-19 13:18:06 +0900

    [update] sessionコントローラの追加

---
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
+  private
+    def resource_params
+      params.require(:session).permit(:email, :password)
+    end
+end
