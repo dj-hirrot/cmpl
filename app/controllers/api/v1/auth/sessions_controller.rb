749df94b616497d45ca0212ebfca5ef6ab6d74f0
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-20 00:35:43 +0900

    [update] strong parameters

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -12,6 +12,6 @@ class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
 
   private
     def resource_params
-      params.require(:session).permit(:login, :password)
+      params.require(:session).permit(:username, :email, :password)
     end
 end
