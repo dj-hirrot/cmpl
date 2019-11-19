9f7c88a31cea89579cbc9e2843ef8c018833b289
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-19 14:15:44 +0900

    [update] sessionsをカスタマイズ

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   namespace :api, { format: :json } do
     scope :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-        registrations: 'api/v1/auth/registrations'
+        registrations: 'api/v1/auth/registrations',
+        sessions: 'api/v1/auth/sessions'
       }
     end
   end
